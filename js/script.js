648e4eae4650e5318b2346c1b6c4d53504c3c5b1
DiegoKasta <diegokasta23@gmail.com>
2023-01-29 20:17:34 -0500

    Ajustando estilo de la pagina

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 
 const vocales =["a","e","i","o","u"];
 const cambios =["ai","enter","imes","ober","ufat"];
-let textIn = document.getElementById("areain");
+const random_margin = ["-5px", "1px", "5px", "10px", "7px"];
+const random_colors = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328"];
+const random_degree = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
+let index = 0;
 
 function cambiandotexto(a,b) {
-    let text=textIn.value;
-    if (!text || /[^\w\s]/.test(text) || /[A-Z]/.test(text)) {
-        alert("Debe ingresar solo caracteres validos y letra en minuscula");
-        areain.focus();
+    let text=document.getElementById("areain").value;
+    if (!text||/[^\w\s]/.test(text)||/[0-9A-Z]/.test(text)) {
+        aviso(`alert-danger`,`<strong>Error !</strong><br>ingresa solo caracteres validos`);
+        document.getElementById("areain").value="";
+        document.getElementById("areain").focus;
     }
     else{
         for (let i = 0; i< a.length; i++) {
@@ -18,35 +22,50 @@ function cambiandotexto(a,b) {
             const e = b[i];
             text=text.replaceAll(i+1,e);
         }
-        // document.getElementById("areaout").value=text;
-        areaout.value=text;
-        areain.value="";
-        // document.getElementById("areain").value="";
+        document.getElementById("areain").value="";
+        document.getElementById("copia").value=text;
+        createStickyNote(text);
+        // document.getElementById("button_copy").setAttribute("style",`display:inline`);
+        document.getElementById("button_copy").style.display="inline"
     }
 }
-function copiar(){
-    let textcopy = document.getElementById("areaout");
-    console.log(textcopy.value);
+
+function copy(){
+    let textcopy = document.getElementById("copia");
     textcopy.select();
     textcopy.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(textcopy.value);
+    aviso('alert-info','<strong>Excelente !</strong><br>texto copiado');
 }
 
-// function desencriptar() {
-//     let text=textIn.value;
-//     for (let i = 0; i< cambios.length; i++) {
-//         const element = cambios[i];
-//         text=text.replaceAll(element,i++);
-//     }
-    
-//     for (let i = 0; i< vocales.length; i++) {
-//         const e = vocales[i];
-//         text=text.replaceAll(i++,e);
-//     }
-//     document.getElementById("areaout").value=text;
-// }
+createStickyNote = (text) => {
+    let note = document.createElement("div");
+    let details = document.createElement("div");
+    let noteText = document.createElement("h1");
 
+    note.className = "note";
+    details.className = "details";
+    noteText.textContent = text;
 
-document.getElementById("encript").onclick=function() {cambiandotexto(vocales,cambios)};
-document.getElementById("decript").onclick=function() {cambiandotexto(cambios,vocales)};
-document.getElementById("copy").onclick=copiar;
+    details.appendChild(noteText);
+    note.appendChild(details);
+
+    if(index > random_colors.length - 1)
+        index = 0;
+
+    note.setAttribute("style", `margin:${random_margin[Math.floor(Math.random() * random_margin.length)]}; background-color:${random_colors[index++]}; transform:${random_degree[Math.floor(Math.random() * random_degree.length)]}`);
+
+    document.querySelector("#all_notes").appendChild(note);
+}
+
+aviso=(tipo, contenido)=>{
+    document.querySelector("#modal").style.display = "block";
+    document.querySelector("#modal").innerHTML=`<div id="inner_modal"><div class="alert alert-dismissible ${tipo}"><button type="button" id="button_close" class="btn-close" data-bs-dismiss="alert"></button>${contenido}</div></div>`;
+    document.getElementById("button_close").addEventListener("click", () => {
+        document.querySelector("#modal").style.display = "none";
+    });
+}
+
+document.getElementById("button_encript").onclick=function() {cambiandotexto(vocales,cambios)};
+document.getElementById("button_decript").onclick=function() {cambiandotexto(cambios,vocales)};
+document.getElementById("button_copy").onclick=copy;
